be5ed5a59649237186efdaac9209ee055ffc8bae
Daniel <danielreyesaleman4@unitec.edu>
2023-03-10 13:38:14 -0600

    Clases hechas

---
+++ src/danielreyes_lab7p2/Archivo.java
@@ -0,0 +1,59 @@
+
+package danielreyes_lab7p2;
+
+
+public class Archivo {
+    private String nombre;
+    private String link;
+    private String extension;
+    private double tamano;
+
+    public Archivo() {
+    }
+
+    public Archivo(String nombre, String link, String extension, double tamano) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public double getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(double tamano) {
+        this.tamano = tamano;
+    }
+
+    @Override
+    public String toString() {
+        return "Archivo{" + "nombre=" + nombre + ", link=" + link + ", extension=" + extension + ", tamano=" + tamano + '}';
+    }
+    
+    
+}
---
+++ src/danielreyes_lab7p2/Carpepta.java
@@ -0,0 +1,59 @@
+
+package danielreyes_lab7p2;
+
+import java.util.ArrayList;
+
+
+public class Carpepta {
+    private String nombre;
+    private String link;
+    private ArrayList<Archivo> archivos = new ArrayList();
+    private ArrayList<Carpepta> carpetas = new ArrayList();
+
+    public Carpepta() {
+    }
+
+    public Carpepta(String nombre, String link) {
+        this.nombre = nombre;
+        this.link = link;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ArrayList<Archivo> getArchivos() {
+        return archivos;
+    }
+
+    public void setArchivos(ArrayList<Archivo> archivos) {
+        this.archivos = archivos;
+    }
+
+    public ArrayList<Carpepta> getCarpetas() {
+        return carpetas;
+    }
+
+    public void setCarpetas(ArrayList<Carpepta> carpetas) {
+        this.carpetas = carpetas;
+    }
+
+    @Override
+    public String toString() {
+        return "Carpepta{" + "nombre=" + nombre + ", link=" + link + ", archivos=" + archivos + ", carpetas=" + carpetas + '}';
+    }
+    
+    
+}
